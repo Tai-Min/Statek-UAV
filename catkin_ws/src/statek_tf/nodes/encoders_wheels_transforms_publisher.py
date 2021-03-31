43b9cc810d939e349734fc5351d623c271fbf921
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2021-03-31 17:57:10 +0200

    Add tf to statek_config, change Encoder msg

--- catkin_ws/src/statek_tf/nodes/encoders_wheels_transforms_publisher.py
+++ catkin_ws/src/statek_tf/nodes/encoders_wheels_transforms_publisher.py
@@ -6,13 +6,13 @@ import geometry_msgs.msg
 
 from statek_msgs.msg import Encoder
 
-def send_transform(link, x, y, z, rx):
+def send_transform(parent_link, child_link, x, y, z, rx):
     br = tf2_ros.TransformBroadcaster()
     t = geometry_msgs.msg.TransformStamped()
 
     t.header.stamp = rospy.Time.now()
-    t.header.frame_id = statek_name + "/base_link"
-    t.child_frame_id = link
+    t.header.frame_id = parent_link
+    t.child_frame_id = child_link
     t.transform.translation.x = x
     t.transform.translation.y = y
     t.transform.translation.z = z
@@ -25,25 +25,46 @@ def send_transform(link, x, y, z, rx):
     br.sendTransform(t)
 
 def encoder_callback(msg, args):
-    side = args[0]
-    statek_name = args[1]
+    statek_name = args[0]
+    tf_dict = args[1]
 
-    motor_link = statek_name + "/motors/" + side + "_link"
-    front_wheel_link = statek_name + "/wheels/" + side + "_front_link"
-    back_wheel_link = statek_name + "/wheels/" + side + "_back_link"
+    parent_link = statek_name + "/base_link"
 
-    y_offset_motor = -0.262682 if side == "right" else 0.262682
-    y_offset_wheel = -0.317682 if side == "right" else 0.317682
-
-    send_transform(motor_link, -0.057, y_offset_motor, 0.0, msg.position)
-    send_transform(front_wheel_link, 0.167807, y_offset_wheel, -0.03126, msg.position)
-    send_transform(back_wheel_link, -0.252682, y_offset_wheel, -0.03126, msg.position)
+    send_transform(parent_link, tf_dict["motor_link"], tf_dict["motor"][0], tf_dict["motor"][1], tf_dict["motor"][2], msg.position)
+    send_transform(parent_link, tf_dict["front_wheel_link"], tf_dict["front_wheel"][0], tf_dict["front_wheel"][1], tf_dict["front_wheel"][2], msg.position)
+    send_transform(parent_link, tf_dict["back_wheel_link"], tf_dict["back_wheel"][0], tf_dict["back_wheel"][1], tf_dict["back_wheel"][2], msg.position)
 
 rospy.init_node("dynamic_transform_publisher", anonymous=True)
 
 statek_name = rospy.get_param("~statek_name", "statek")
 
-rospy.Subscriber("/" + statek_name + "/real_time/motors/left/encoder", Encoder, encoder_callback, ("left", statek_name))
-rospy.Subscriber("/" + statek_name + "/real_time/motors/right/encoder", Encoder, encoder_callback, ("right", statek_name))
+left_motor_tf = rospy.get_param("~base_to_left_motor", [0, 0, 0, 0, 0, 0])
+right_motor_tf = rospy.get_param("~base_to_right_motor", [0, 0, 0, 0, 0, 0])
+
+left_back_wheel_tf = rospy.get_param("~base_to_left_back_wheel", [0, 0, 0, 0, 0, 0])
+left_front_wheel_tf = rospy.get_param("~base_to_left_front_wheel", [0, 0, 0, 0, 0, 0])
+right_back_wheel_tf = rospy.get_param("~base_to_right_back_wheel", [0, 0, 0, 0, 0, 0])
+right_front_wheel_tf = rospy.get_param("~base_to_right_front_wheel", [0, 0, 0, 0, 0, 0])
+
+left_side_tf = {
+    "motor": left_motor_tf,
+    "back_wheel": left_back_wheel_tf,
+    "front_wheel": left_front_wheel_tf,
+    "motor_link": statek_name + "/motors/left_link",
+    "back_wheel_link": statek_name + "/wheels/left_back_link",
+    "front_wheel_link": statek_name + "/wheels/left_front_link"
+}
+
+right_side_tf = {
+    "motor": right_motor_tf,
+    "back_wheel": right_back_wheel_tf,
+    "front_wheel": right_front_wheel_tf,
+    "motor_link": statek_name + "/motors/right_link",
+    "back_wheel_link": statek_name + "/wheels/right_back_link",
+    "front_wheel_link": statek_name + "/wheels/right_front_link"
+}
+
+rospy.Subscriber("/" + statek_name + "/real_time/motors/left/encoder", Encoder, encoder_callback, (statek_name, left_side_tf))
+rospy.Subscriber("/" + statek_name + "/real_time/motors/right/encoder", Encoder, encoder_callback, (statek_name, right_side_tf))
 
 rospy.spin()
