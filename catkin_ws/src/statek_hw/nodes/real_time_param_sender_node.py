4995859c6dd1de83412a81817b464b141bfdb294
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2021-04-08 18:34:22 +0200

    Add imu and odom to param sender node

--- catkin_ws/src/statek_hw/nodes/real_time_param_sender_node.py
+++ catkin_ws/src/statek_hw/nodes/real_time_param_sender_node.py
@@ -3,7 +3,12 @@
 import time
 import rospy
 from statek_msgs.srv import SetMotorParams
+from statek_msgs.srv import SetImuParams
+from statek_msgs.srv import SetOdomParams
+
 from statek_msgs.srv import SetMotorParamsRequest
+from statek_msgs.srv import SetImuParamsRequest
+from statek_msgs.srv import SetOdomParamsRequest
 
 def send_motor_params(namespace, param_service, params):
     full_service_name = "/" + namespace + param_service
@@ -22,14 +27,65 @@ def send_motor_params(namespace, param_service, params):
         res = service(req)
         if res.success == False:
             rospy.logwarn("Failed to send parameters.")
+            return False
     except:
         rospy.logwarn("Exception occured! Failed to send parameters.")
+        return False
+    return True
+
+def send_imu_params(namespace, param_service, params):
+    full_service_name = "/" + namespace + param_service
+    rospy.loginfo("Sending parameters to %s" % (full_service_name))
+    rospy.wait_for_service(full_service_name)
+    try:
+        service = rospy.ServiceProxy(full_service_name, SetImuParams)
+
+        req = SetImuParamsRequest()
+        req.imu_update_rate_ms = params["imu_update_rate_ms"]
+        req.acc_bias = params["acc_bias"]
+        req.gyro_bias = params["gyro_bias"]
+        req.mag_bias = params["mag_bias"]
+        req.mag_scale = params["mag_scale"]
+        req.magnetic_declination = params["magnetic_declination"]
+
+        res = service(req)
+        if res.success == False:
+            rospy.logwarn("Failed to send parameters.")
+            return False
+    except:
+        rospy.logwarn("Exception occured! Failed to send parameters.")
+        return False
+    return True
+
+def send_odom_params(namespace, param_service, params):
+    full_service_name = "/" + namespace + param_service
+    rospy.loginfo("Sending parameters to %s" % (full_service_name))
+    rospy.wait_for_service(full_service_name)
+    try:
+        service = rospy.ServiceProxy(full_service_name, SetOdomParams)
+
+        req = SetOdomParamsRequest()
+        req.wheel_radius = params["wheel_radius"]
+        req.distance_between_wheels = params["distance_between_wheels"]
+        req.odom_update_rate_ms = params["odom_update_rate_ms"]
+
+        res = service(req)
+        if res.success == False:
+            rospy.logwarn("Failed to send parameters.")
+            return False
+    except:
+        rospy.logwarn("Exception occured! Failed to send parameters.")
+        return False
+    return True
+
 
 rospy.init_node("real_time_param_sender", anonymous=True)
 
 statek_name = rospy.get_param("~statek_name", "statek")
 left_motor_param_service_name = rospy.get_param("~left_motor_param_service_name", "/real_time/motors/left/set_params")
 right_motor_param_service_name = rospy.get_param("~right_motor_param_service_name", "/real_time/motors/right/set_params")
+imu_param_service_name = rospy.get_param("~imu_param_service_name", "/real_time/imu/set_params")
+odom_param_service_name = rospy.get_param("~odom_param_service_name", "/real_time/odom/set_params")
 
 left_motor_pid = rospy.get_param("~left_motor_pid", [0,0,0])
 right_motor_pid = rospy.get_param("~right_motor_pid", [0,0,0])
@@ -50,11 +106,35 @@ right_motor_params = {
     "kd": right_motor_pid[2]
 }
 
+imu_params = {
+    "imu_update_rate_ms": rospy.get_param("~imu_update_rate_ms", 0),
+    "acc_bias": rospy.get_param("~acc_bias", [0,0,0]),
+    "gyro_bias": rospy.get_param("~gyro_bias", [0,0,0]),
+    "mag_bias": rospy.get_param("~mag_bias", [0,0,0]),
+    "mag_scale": rospy.get_param("~mag_scale", [0,0,0]),
+    "magnetic_declination": rospy.get_param("~magnetic_declination", 0)
+}
+
+odom_params = {
+    "wheel_radius": rospy.get_param("~wheel_radius", 0),
+    "distance_between_wheels": rospy.get_param("~distance_between_wheels", 0),
+    "odom_update_rate_ms": rospy.get_param("odom_update_rate_ms", 0)
+}
+
 time.sleep(20)
-send_motor_params(statek_name, left_motor_param_service_name, left_motor_params)
-time.sleep(1)
-send_motor_params(statek_name, right_motor_param_service_name, right_motor_params)
-time.sleep(1)
-# TODO: imu params...
-time.sleep(1)
-# TODO: odom params...
+
+while(not send_motor_params(statek_name, left_motor_param_service_name, left_motor_params)):
+    time.sleep(5)
+time.sleep(2)
+
+while(not send_motor_params(statek_name, right_motor_param_service_name, right_motor_params)):
+    time.sleep(5)
+time.sleep(2)
+
+while(not send_imu_params(statek_name, imu_param_service_name, imu_params)):
+    time.sleep(5)
+time.sleep(2)
+
+while(not send_odom_params(statek_name, odom_param_service_name, odom_params)):
+    time.sleep(5)
+time.sleep(2)
