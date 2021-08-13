0f0a14f6885f28c1ac793d549e013bcf63ac1cf7
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2021-08-13 17:51:52 +0200

    Working on leg detector

--- catkin_ws/src/statek_ml/nodes/leg_detector.py
+++ catkin_ws/src/statek_ml/nodes/leg_detector.py
@@ -3,9 +3,10 @@
 import rospy
 import rospkg
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-import tensorflow as tf
+#import tensorflow as tf
 import sys
+import cv2
+import numpy as np
 
 def load_model():
     model_path = rospkg.RosPack().get_path("statek_ml")
@@ -15,17 +16,21 @@ def load_model():
     net = __import__("net")
     net = net.PeTraNet()
 
-    ckpt = tf.train.latest_checkpoint(model_path + "/")
-    ckpt = tf.train.Checkpoint(net=net).restore(ckpt).expect_partial()
+    preprocessor = __import__("dataset_preprocessing")
+    preprocessor = preprocessor.preprocess_real_data
 
-    return net
+    #ckpt = tf.train.latest_checkpoint(model_path + "/.tf_ckpts")
+    #ckpt = tf.train.Checkpoint(net=net).restore(ckpt).expect_partial()
+
+    return net, preprocessor
 
 def scan_callback(msg):
-    
-scan_callback.net = load_model()
+    frame = np.array(list(msg.data))
+    frame = np.reshape(frame, (1, msg.height, msg.width))
+    print(frame.shape)
 
-
-bridge = CvBridge()
+    #frame = scan_callback.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    #print(frame)
 
 # Init ROS.
 rospy.init_node('lidar_fconv_dataset_collector')
@@ -33,3 +38,10 @@ statek_name = rospy.get_param("~statek_name", "statek")
 
 # Init lidar image subscriber.
 rospy.Subscriber("/" + statek_name + "/laser/scan_img", Image, scan_callback)
+
+# Stuff for laser image callback.
+#scan_callback.bridge = CvBridge()
+scan_callback.leg_publisher = rospy.Publisher("/" + statek_name + "/laser/legs_img", Image, queue_size=1)
+#scan_callback.net = load_model()
+
+rospy.spin()
