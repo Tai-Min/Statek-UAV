526579aa2cb4190a46a498ed1c2d62523dc7ae34
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2021-08-18 17:24:06 +0200

    Add bresenham filling to lidar_to_img node, add kalman filtering to leg_detector

--- catkin_ws/src/statek_hw/src/laser_to_img/laser_to_img_node.cpp
+++ catkin_ws/src/statek_hw/src/laser_to_img/laser_to_img_node.cpp
@@ -11,9 +11,19 @@ namespace
     bool receivedFirstMsg = false;
     double imgWidthMeters;
     double imgHeightMeters;
+    double bresenhamScalingParam;
     int imgWidth;
     int imgHeight;
     sensor_msgs::Image scanImg;
+}
+
+double norm(int y, int x){
+    return sqrt(pow(y, 2) + pow(x, 2));
+}
+
+double distanceFromCenter(int y, int x)
+{
+    return norm(y - imgHeight / 2, x - imgWidth / 2);
 }
 
 void onNewScan(const sensor_msgs::LaserScan::ConstPtr &scan)
@@ -25,6 +35,9 @@ void onNewScan(const sensor_msgs::LaserScan::ConstPtr &scan)
     double currentAngle = scan->angle_min;
     double increment = scan->angle_increment;
 
+    std::vector<cv::Point> obstacles;
+
+    // Place obstacles.
     for (unsigned int i = 0; i < scan->ranges.size(); i++)
     {
         double ray = scan->ranges[i];
@@ -54,10 +67,34 @@ void onNewScan(const sensor_msgs::LaserScan::ConstPtr &scan)
         }
 
         std::cout << "ADD" << std::endl;
-        scanImgMat.at<uint8_t>(cv::Point(y, x)) = 255;
+        scanImgMat.at<uint8_t>(y, x) = 255;
+        obstacles.push_back(cv::Point(x, y));
 
         currentAngle += increment;
     }
+
+    // Fill gaps.
+    for (unsigned int i = 0; i < obstacles.size(); i++)
+    {
+        // Get distance from center to check what maximum distance
+        // between two obstacle pixels is allowed.
+        double distanceCenter = distanceFromCenter(obstacles[i].y, obstacles[i].x);
+        double maxAllowedDistance = bresenhamScalingParam * distanceCenter;
+
+        // Fill space between two close enough obstacles.
+        for (unsigned int j = i + 1; j < obstacles.size(); j++)
+        {
+            double distance = norm(obstacles[i].y - obstacles[j].y, obstacles[i].x - obstacles[j].x);
+
+            if (distance <= maxAllowedDistance){
+                cv::line(scanImgMat, obstacles[i], obstacles[j], 255);
+            }
+                
+        }
+    }
+
+    cv::rotate(scanImgMat, scanImgMat, cv::ROTATE_90_CLOCKWISE);
+    cv::flip(scanImgMat, scanImgMat, 1);
 
     cv_bridge::CvImage imgBridge = cv_bridge::CvImage(scanImg.header, sensor_msgs::image_encodings::MONO8, scanImgMat);
     imgBridge.toImageMsg(scanImg);
@@ -80,6 +117,9 @@ int main(int argc, char **argv)
     std::string laserImgTopic;
     std::string cameraInfoTopic;
 
+    double bresenhamMaxDistance;
+    double bresenhamMaxPixels;
+
     nh.param<std::string>("statek_name", statekName, "statek");
     nh.param<std::string>("laser_img_frame", laserImgFrame, statekName + "/laser/laser_link");
     nh.param<std::string>("laser_topic", laserTopic, "/" + statekName + "/laser/scan");
@@ -90,6 +130,9 @@ int main(int argc, char **argv)
     nh.param<double>("height_meters", imgHeightMeters, 6.0);
     nh.param<int>("width", imgWidth, 256);
     nh.param<int>("height", imgHeight, 256);
+    nh.param<double>("bresenham_max_pixels", bresenhamMaxPixels, 3);
+    bresenhamMaxDistance = sqrt(imgWidth * imgWidth + imgHeight * imgHeight);
+    bresenhamScalingParam = bresenhamMaxPixels / bresenhamMaxDistance;
 
     int updateRate;
     nh.param<int>("fps", updateRate, 15);
--- catkin_ws/src/statek_ml/nodes/leg_detector.py
+++ catkin_ws/src/statek_ml/nodes/leg_detector.py
@@ -3,10 +3,14 @@ from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 import rospy
 import rospkg
+from visualization_msgs.msg import Marker, MarkerArray
 from sensor_msgs.msg import Image
 import tensorflow as tf
 import sys
 import numpy as np
+import time
+import math
+import threading
 
 # Load _get_legs function from petranet's preprocessing.
 r = rospkg.RosPack()
@@ -16,53 +20,161 @@ sys.path.insert(1, path)
 get_legs = __import__("dataset_processing")
 get_legs = get_legs._get_legs
 
+data_path = r.get_path("statek_ml") + "/data"
+
 class LegKalman():
-    pass
+    def __init__(self, leg_position, process_variance, measurement_variance):
+        # @brief Class constructor.
+        # @param leg_position Initial leg position (y, x).
+        # @param process_variance Process variance.
+        # @param measurement_variance Measurement variance.
+    
+        # Parameters.
+        self.Q = np.array([[process_variance, 0], [0, process_variance]])
+        self.R = np.array([[measurement_variance, 0],[0, measurement_variance]])
+
+        # Initial conditions.
+        y = leg_position[0]
+        x = leg_position[1]
+        self.a_posteriori_xhat = np.array([[y], [x]])
+        self.a_posteriori_P = self.Q
+
+    def update(self, velocity_measurement, position_measurement, dt):
+        # @brief Update filter.
+        # @param velocity_measurement Velocity measurement based on current position measurement
+        # and previous estimate (vy, vx).
+        # @param position_measurement Current position measurement.
+        # @param Time passed since last update.
+        # @return Position estimate (y, x).
+
+        vy = velocity_measurement[0]
+        vx = velocity_measurement[1]
+        
+        y = position_measurement[0]
+        x = position_measurement[1]
+        u = np.array([[vy], [vx]])
+        z = np.array([[y], [x]])
+        B = np.array([[dt, 0], [0, dt]])
+
+        # Predict.
+        a_priori_xhat = self.a_posteriori_xhat + B @ u
+        a_priori_P = self.a_posteriori_P + self.Q
+
+        # Update.
+        innovation = z - a_priori_xhat
+        innovation_covariance = a_priori_P + self.R
+        K = a_priori_P @ np.transpose(innovation_covariance)
+        self.a_posteriori_xhat = a_priori_xhat + K @ innovation
+        self.a_posteriori_P = (np.identity(2) - K) @ a_priori_P
+
+        y = self.a_posteriori_xhat[0]
+        x = self.a_posteriori_xhat[1]
+
+        return (y[0], x[0])
 
 class Leg():
     
     def __init__(self, leg, forget_time, max_distance, process_variance, measurement_variance):
         # @brief Class constructor.
-        # @param leg Initial coordinates of the leg in pixel domain.
-        # @param forget_time Period of time in which update() should success at least once. 
+        # @param leg Initial coordinates of the leg in meters [y, x].
+        # @param forget_time Period of time in seconds in which update() should success at least once. 
         # If there was no succesfull update in this time period then alive() will return False
         # and leg should be removed.
         # @param max_distance Maximum allowed euclidean distance in meters between current leg position (from Kalman)
         # and leg candidates coordinates. This distance assumes update time of 1 second.
         # @param process_variance Process variance for Kalman filter.
         # @param measurement_variance Measurement variance for Kalman filter.
-        self.position = leg
+
+        # Params.
+        self.forget_time = forget_time
+        self.max_distance = max_distance
+
+        # First update.
+        self.filter = LegKalman(leg, process_variance, measurement_variance)
+        self.last_update = time.time()
+        self.estimated_velocity = (0,0)
+        self.estimated_position = leg
+
+    def _get_distance(self, candidate):
+        # @brief Get candidate's distance from current leg's estimated position.
+        # @param candidate Candidate to check (y, x).
+        # @return Distance from candidate to current's position.
+
+        return math.sqrt((candidate[0] - self.estimated_position[0])**2 + (candidate[1] - self.estimated_position[1])**2)
+
+    def _find_candidate(self, leg_candidates):
+        # @brief Find most promising leg candidate.
+        # @param leg_candidates List od leg candidates [(y,x), (y,x), ...].
+        # @return Tuple in which the first element is found distance and second is it's index.
+
+        distances = [self._get_distance(candidate) for candidate in leg_candidates]
+        min_distance = min(distances)
+        return (min_distance, distances.index(min_distance))
+
 
     def update(self, leg_candidates):
         # @brief Update leg object.
-        # @param List of legs detected from PeTraNet prediction. 
+        # @param List of legs detected from PeTraNet prediction [(y, x), (y, x), ...]. 
         # If the update succeeds then this function will remove most promising 
         # leg candidate.
         # @param dt Time passed since last call to update()
         # @return True on success.
-        pass
+
+        if len(leg_candidates) == 0:
+            return leg_candidates
+
+        # Check whether any candidate is in range.
+        candidate_distance, candidate_index = self._find_candidate(leg_candidates)
+        if candidate_distance > self.max_distance:
+            return leg_candidates
+
+        measured_position = tuple(leg_candidates[candidate_index])
+
+        # A priori velocity.
+        passed = time.time() - self.last_update
+        if passed == 0:
+            return leg_candidates
+
+        measured_velocity_y = (measured_position[0] - self.estimated_position[0]) / passed
+        measured_velocity_x = (measured_position[1] - self.estimated_position[1]) / passed
+        measured_velocity = (measured_velocity_y, measured_velocity_x)
+        
+        # Position estimation.
+        previous_position_estimate = self.estimated_position
+        self.estimated_position = self.filter.update(measured_velocity, measured_position, passed)
+
+        # A posteriori velocity.
+        estimated_velocity_y = (self.estimated_position[0] - previous_position_estimate[0]) / passed
+        estimated_velocity_x = (self.estimated_position[1] - previous_position_estimate[1]) / passed
+        self.estimated_velocity = (estimated_velocity_y, estimated_velocity_x)
+
+        del leg_candidates[candidate_index]
+        self.last_update = time.time()
+
+        return leg_candidates
 
     def alive(self):
         # @brief Whether this object should be destroyed.
-        # @return True if should be destroyed.
-        pass
+        # @return True if leg is still alive.
 
-    def position_pixels(self):
-        # @brief Get this leg's position (from Kalman filer).
-        # @return tuple of (y, x) coordinates in pixel domain.
-        pass
+        return (time.time() - self.last_update) < self.forget_time
 
-    def position_meters(self):
-        # @brief Get this leg's position (from Kalman filer).
-        # @return tuple of (y, x) coordinates in SI domain.
-        pass
+    def position(self):
+        # @brief Get this leg's estimated position.
+        # @return tuple of (y, x) coordinates.
 
-    def velocity_meters(self):
-        # @brief Get this leg's velocity (from Kalman filer).
-        # @return tuple of (y, x) velocities in SI domain.
-        pass
+        return self.estimated_position
+
+    def velocity(self):
+        # @brief Get this leg's estimated velocity.
+        # @return tuple of (y, x) velocities.
+
+        return self.estimated_velocity
 
 def load_network():
+    # @brief Load network and it's preprocessing function.
+    # @return (network model, preprocessing function).
+
     model_path = rospkg.RosPack().get_path("statek_ml")
     model_path += "/models/lidar_net"
     sys.path.insert(1, model_path)
@@ -74,15 +186,23 @@ def load_network():
     preprocessor = __import__("dataset_processing")
     preprocessor = preprocessor.preprocess_input_sample
 
-    return net, preprocessor
+    return (net, preprocessor)
 
 # Can't get cv_bridge to work on python3 and Jeston Nano D:
 def msg_to_img(msg):
+    # @brief Convert message to image suitable for preprocessing.
+    # @param msg Message to convert.
+    # @return Converted message.
+
     frame = np.array(list(msg.data))
     frame = np.reshape(frame, (1, msg.height, msg.width))
     return frame
 
 def prediction_to_cv(prediction):
+    # @brief Convert prediction (0 - 1 float) to cv matrix (0 - 255 uint8).
+    # @param prediction Prediction to convert.
+    # @return Converted prediction.
+
     prediction = prediction[0].numpy()
     prediction = np.round(prediction)
     prediction *= 255.0
@@ -90,37 +210,54 @@ def prediction_to_cv(prediction):
     prediction = np.squeeze(prediction)
     return prediction
 
-def to_meters(y, x, pixel_height, pixel_width, height, width):
-    # @brief convert coordinates of pixel to 
-    pass
+def to_meters(leg, height_pixels, width_pixels, height, width):
+    # @brief convert coordinates from pixel to meters.
+    # @param leg Leg to convert [y, x].
+    # @param height_pixels Image's height in pixels.
+    # @param width_pixels Image's width in pixels.
+    # @param height Image's height in meters.
+    # @param width Image's width in meters.
 
-def scan_callback(msg):
-    # Make prediction.
-    frame = msg_to_img(msg)
-    frame = scan_callback.preprocessor(frame)
-    prediction = scan_callback.net(frame)
-    prediction = prediction_to_cv(prediction)
-    leg_candidates = get_legs(prediction)
+    y = float(leg[0])
+    x = float(leg[1])
 
-    # Update legs.
-    for leg in scan_callback.legs:
-        leg.update(leg_candidates)
+    y -= height_pixels / 2.0
+    x -= width_pixels / 2.0
 
-    # Remove dead legs.
-    scan_callback.legs = [leg for leg in scan_callback.legs if leg.alive()]
+    y *= (height / height_pixels)
+    x *= (width / width_pixels)
 
-    # Generate new legs from candidates that were not consumed by already
-    # existing legs.
-    for candidate in leg_candidates:
-        scan_callback.legs.append(Leg(candidate, scan_callback.forget_time, scan_callback.max_distance,
-        scan_callback.process_variance, scan_callback.measurement_variance))
+    return [y, x]
 
-    msg.data = prediction.flatten().tolist()
-    msg.header.stamp = rospy.Time.now()
-    scan_callback.publisher.publish(msg)
+def to_meters_arr(legs, height_pixels, width_pixels, height, width):
+    return [to_meters(leg, height_pixels, width_pixels, height, width) for leg in legs]
 
-    print(leg_candidates)
+def get_marker(position, id):
+    marker = Marker()
+    marker.header.frame_id = "statek/laser/laser_link"
+    marker.header.stamp = rospy.Time.now()
+    marker.ns = "legs"
+    marker.id = id
+    marker.action = Marker.ADD
+    marker.type = Marker.SPHERE
+    marker.lifetime.secs = 3
+    marker.pose.position.x = position[0]
+    marker.pose.position.y = position[1]
+    marker.pose.position.z = 0.15
+    marker.scale.x = 0.2
+    marker.scale.y = 0.2
+    marker.scale.z = 0.2
+    marker.color.a = 1
+    marker.color.r = 1
+    marker.color.b = 0
+    marker.color.g = 1
+    return marker
 
+def scan_callback(new_msg):
+    global msg, msg_lock
+    with msg_lock:
+        msg = new_msg
+        msg_arrived = True
 
 # Limit memory usage as it's shared with CPU.
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -135,16 +272,69 @@ rospy.init_node('lidar_fconv_dataset_collector')
 statek_name = rospy.get_param("~statek_name", "statek")
 
 
-# Stuff for laser image callback.
-scan_callback.publisher = rospy.Publisher("/" + statek_name + "/laser/legs_img", Image, queue_size=1)
-scan_callback.legs = []
-scan_callback.forget_time = rospy.get_param("~leg_forget_time", "/real_time/motors/left/encoder")
-scan_callback.max_distance = rospy.get_param("~candidate_max_distance", "/real_time/motors/left/encoder")
-scan_callback.process_variance = rospy.get_param("~leg_process_variance", "/real_time/motors/left/encoder")
-scan_callback.measurement_variance = rospy.get_param("~leg_measurement_variance", "/real_time/motors/left/encoder")
-scan_callback.net, scan_callback.preprocessor = load_network()
+# Stuff for .
+publisher = rospy.Publisher("/" + statek_name + "/laser/hoomans", MarkerArray, queue_size=1)
+msg = None
+msg_lock = threading.Lock()
+msg_arrived = False
+legs = []
+width_meters = rospy.get_param("~width_meters", 5.12)
+height_meters = rospy.get_param("~height_meters", 5.12)
+width_pixels = rospy.get_param("~width_pixels", 256)
+height_pixels = rospy.get_param("~height_pixels", 256)
+fps = rospy.get_param("~fps", 15)
+forget_time = rospy.get_param("~leg_forget_time", 2)
+max_distance = rospy.get_param("~candidate_max_distance", 1.0)
+process_variance = rospy.get_param("~leg_process_variance", 0.01)
+measurement_variance = rospy.get_param("~leg_measurement_variance", 0.01)
+net, preprocessor = load_network()
 
 # Init lidar image subscriber.
-rospy.Subscriber("/" + statek_name + "/laser/scan_img", Image, scan_callback, queue_size=1)
+rospy.Subscriber("/" + statek_name + "/laser/scan_img", Image, scan_callback, queue_size=1, buff_size=65536*2)
 
-rospy.spin()
+rate = rospy.Rate(fps)
+while not rospy.is_shutdown():
+    if msg_arrived == False:
+        continue
+
+    # Preprocess data.
+    with msg_lock:
+        frame = msg_to_img(msg)
+        msg_arrived = False
+    frame = preprocessor(frame)
+
+    # Make prediction.
+    prediction = net(frame)
+    prediction = prediction_to_cv(prediction)
+
+    # Extract legs.
+    leg_candidates = get_legs(prediction)
+    #leg_candidates = [[20,10],[100,120], [25,33]]
+    if len(leg_candidates) == 0:
+        continue
+
+    leg_candidates = to_meters_arr(leg_candidates,
+                                    height_pixels,width_pixels,
+                                    height_meters, width_meters)
+
+    # Update legs.
+    cntr = 0
+    markers = MarkerArray()
+    for leg in legs:
+        leg_candidates = leg.update(leg_candidates)
+        markers.markers.append(get_marker(leg.position(), cntr))
+        cntr+=1
+        
+    publisher.publish(markers)
+    rospy.logwarn("pub!")
+
+    # Remove dead legs.
+    legs = [leg for leg in legs if leg.alive()]
+
+    # Generate new legs from candidates that were not consumed by already
+    # existing legs.
+    for candidate in leg_candidates:
+        legs.append(Leg(candidate, forget_time, max_distance,
+        process_variance, measurement_variance))
+
+    rate.sleep()
